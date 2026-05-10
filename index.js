f8ae1febbb3e319e56c9b2bd0361c7737b0a536f
Jennifer Cobb <jennifercobb829@gmail.com>
2026-05-10 11:58:53 -0400

    Updating

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let isModalOpen = false;
 function toggleModal() {
-    document.body.classList.toggle("modal--open")
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += "modal--open";
 }
