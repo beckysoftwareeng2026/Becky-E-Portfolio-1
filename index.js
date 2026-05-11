9be663c85a9c3d61def19918e675cbd0349b84a4
Jennifer Cobb <jennifercobb829@gmail.com>
2026-05-11 14:45:16 -0400

    Updating

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ function toggleModal() {
     isModalOpen = true;
     document.body.classList += "modal--open";
 }
+
+function contact() {
+    event.preventDefault();
+    const loading = document.querySelector('modal__overlay--success')
+    const success = document.querySelector('modal__overlay--loading')
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+        .sendForm(
+            'service_g0yjped',
+            'template_8njakj3',
+            event.target,
+            'f6i8YNXr8vT28SPdI'
+        ).then(() => {
+            loading.classList.remove(" modal__overlay--visible")
+            success.classList += loading.classList += " modal__overlay--visible"
+        }).catch(() => { }
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+            "The email service is temporarily unavailable. Please contact me directly @ jennifercobb829@gmail.com"
+        )
+        })
+
+
+setTimeout(() => {
+
+    console.log('it worked 1')
+}, 1000);
+}
+
